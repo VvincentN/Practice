a9c89a9952fd3f3fd040fb52083deef409943bd0
CherryTea <105888606+VvincentN@users.noreply.github.com>
2024-09-09 10:02:51 +0300

    1st try

---
+++ Practice1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    float d1, d2, r1, r2, S1, S2, coeff;
+    const float pi = 3.14;
+
+    cout << "Введите диаметр окружности из рецепта: ";
+    cin >> d1;
+    cout << "Введите диаметр формы для перерасчёта: ";
+    cin >> d2;
+
+    r1 = d1 / 2;
+    r2 = d2 / 2;
+
+    S1 = pi * (r1 * r1);
+    S2 = pi * (r2 * r2);
+
+    coeff = S2 / S1;
+
+    cout << "Коэффицент для перерасчёта ингридиентов равен " << coeff;
+}
