02519bdc082508868a19cd46dc1b0c459cc4268d
CherryTea <105888606+VvincentN@users.noreply.github.com>
2024-10-18 13:26:32 +0300

    Update ForCollege1.cpp

--- ForCollege1.cpp
+++ ForCollege1.cpp
@@ -1,35 +1,77 @@
 ﻿#include <iostream>
-#include<string>
-#include<cmath>
+using namespace std;
+
+float calcSq(float side) {
+	return side * side;
+}
+float calcRec(float side1, float side2) {
+	return side1 * side2;
+}
+float calcCir(float diametr) {
+	const float pi = 3.14;
+	return pi * (diametr / 2) * (diametr / 2);
+}
 
 int main()
 {
-    int base;
-    double num;
+	system("chcp 1251"); //Переключение кодировки символов в командном интерпретаторе на русскую
 
-    std::string result = "";
-    std::cout << "Enter the number: ";
-    std::cin >> num;
-    std::cout << "Enter the base: ";
-    std::cin >> base;
+	string f1, f2;
+	float a, b, d, r, S1 = 0, S2 = 0;
+	const float pi = 3.14;
 
-    int inty = trunc(num);
-    while (inty >= base) {
-        result.insert(0, std::to_string(inty % base));
-        inty /= base;
-    }
-    result.insert(0, std::to_string(inty));
 
-    double frac = fmod(num, 1);
-    if (fmod(num, 1) != 0.0) {
-        result += ".";
-        for (int i = 0; i < 5; i++) {
-            frac *= base;
-            int fract = static_cast<int>(frac);
-            result += std::to_string(fract);
-            frac -= fract;
-        }
-    }
+	cout << "Введите изначальную форму: ";
+	cin >> f1;
 
-    std::cout << result;
+	if (f1 == "квадратная" || f1 == "квадрат") {
+		cout << "Введите длину стороны изначальной формы: ";
+		cin >> a;
+		S1 = calcSq(a);
+	}
+	else if (f1 == "прямоугольная" || f1 == "прямоугольник") {
+		cout << "Введите длину стороны изначальной формы: ";
+		cin >> a;
+		cout << "Введите ширину стороны изначальной формы: ";
+		cin >> b;
+		S1 = calcRec(a, b);
+	} 
+	else if (f1 == "круглая" || f1 == "круг") {
+		cout << "Введите диаметр окружности изначальной формы: ";
+		cin >> d;
+		S1 = calcCir(d);
+	}
+	else {
+		cout << "ошибочка";
+	}
+
+	cout << "\n";
+	cout << "Введите конечную форму: ";
+	cin >> f2;
+
+	if (f2 == "квадратная" || f2 == "квадрат") {
+		cout << "Введите длину стороны конечной формы: ";
+		cin >> a;
+		S2 = calcSq(a);
+	}
+	else if (f2 == "прямоугольная" || f2 == "прямоугольник") {
+		cout << "Введите длину стороны конечно формы: ";
+		cin >> a;
+		cout << "Введите ширину стороны конечной формы: ";
+		cin >> b;
+		S2 = calcRec(a, b);
+	}
+	else if (f2 == "круглая" || f2 == "круг") {
+		cout << "Введите диаметр окружности конечной формы: ";
+		cin >> d;
+		S2 = calcCir(d);;
+	}
+	else {
+		cout << "Ошибочка";
+	}
+
+
+	cout << "\n";
+	cout << "Пропорция равна: " << S2 / S1;
+
 }
